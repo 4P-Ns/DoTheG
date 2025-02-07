5b81d0de3a732926f3302ac614991c7e54f306ee
DoomchitYJ <poj1004@inha.edu>
2025-02-07 18:06:21 +0900

    feat: 전체 목록 기능 구현

--- src/main/java/controller/ActionFactory.java
+++ src/main/java/controller/ActionFactory.java
@@ -19,15 +19,25 @@ public class ActionFactory {
 	public Action getAction(String command){ //list
 		Action action = null;
 		
-		if(command.equals("main")){	 	
-			action = new MainPageAction();
-		}else if(command.equals("register")){ 
-			action = new RegisterAccountAction();
-		}else if(command.equals("lecture")){  
-			action = new LectureViewAction();
-		}else if(command.equals("write")){ 
-			action = new WriteArticleAction();
-		}else if(command.equals("board")){
+//		if(command.equals("main")){	 	
+//			action = new MainPageAction();
+//		}else if(command.equals("register")){ 
+//			action = new RegisterAccountAction();
+//		}else if(command.equals("lecture")){  
+//			action = new LectureViewAction();
+//		}else if(command.equals("write")){ 
+//			action = new WriteArticleAction();
+//		}else if(command.equals("board")){
+//			action = new AllArticleViewAction();
+//		}else if(command.equals("article_detail")){ 
+//			action = new ArticleDetailViewAction();
+//		}else if(command.equals("update")){ 
+//			action = new UpdateArticleAction();
+//		}else if(command.equals("delete")){ 
+//			action = new DeleteArticleAction();
+//		}
+		
+		if(command.equals("board")){
 			action = new AllArticleViewAction();
 		}else if(command.equals("article_detail")){ 
 			action = new ArticleDetailViewAction();
