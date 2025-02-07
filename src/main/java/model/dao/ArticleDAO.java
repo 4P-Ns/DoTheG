482854247819bbcf016a67534887d0fa62a17b6c
Jeongho427 <fgh2585@gmail.com>
2025-02-07 15:09:41 +0900

    feat: DB 연결을 위한 util Class 생성

--- src/main/java/model/dao/ArticleDAO.java
+++ src/main/java/model/dao/ArticleDAO.java
@@ -1,5 +1,46 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.domain.Article;
+import model.domain.Member;
+import util.DataSourceManager;
+
 public class ArticleDAO {
+
+	/*
+	 * 결과가 1. 정상 - 정상 저장 2. 비정상 - SQLExcption 발생
+	 */
+	public static boolean writeContent(Member member, Article article) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			conn = DataSourceManager.getConnection();
+
+			pstmt = conn.prepareStatement(
+					"INSERT INTO gbook VALUES (seq_article_article_id.nextval, ?, ?, ?, ?, sysdate, sysdate)");
+
+			pstmt.setString(1, member.getNickname());
+			pstmt.setString(2, article.getTitle());
+			pstmt.setString(3, article.getContent());
+			pstmt.setLong(4, member.getFamilyId());
+
+			if (pstmt.executeUpdate() != 0) {
+				return true;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			DataSourceManager.close(conn, pstmt);
+		}
+
+		return false;
+	}
 
 }
--- src/main/java/model/dao/MemberDAO.java
+++ src/main/java/model/dao/MemberDAO.java
@@ -10,31 +10,31 @@ import util.DataSourceManager;
 
 public class MemberDAO {
 	// id로 해당 멤버의 모든 정보 반환
-		public static Member getMember(long memberId) throws SQLException {
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			ResultSet rset = null;
-			Member member = null;
+	public static Member getMember(long memberId) throws SQLException {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		Member member = null;
 
-			try {
-				conn = DataSourceManager.getConnection();
-				pstmt = conn.prepareStatement("select * from member where member_id=?");
-				pstmt.setLong(1, memberId);
-				rset = pstmt.executeQuery();
-				if (rset.next()) {
-					member = new Member(rset.getLong(1), 
-										rset.getString(2),
-										rset.getString(3),
-										rset.getString(4),
-										rset.getString(5),
-										rset.getString(6),
-										rset.getString(7),
-										rset.getString(8),
-										rset.getLong(9));
-				}
-			} finally {
-				DataSourceManager.close(conn, pstmt, rset);
+		try {
+			conn = DataSourceManager.getConnection();
+			pstmt = conn.prepareStatement("select * from member where member_id=?");
+			pstmt.setLong(1, memberId);
+			rset = pstmt.executeQuery();
+			if (rset.next()) {
+				member = new Member(rset.getLong(1), 
+									rset.getString(2), 
+									rset.getString(3), 
+									rset.getString(4),
+									rset.getString(5), 
+									rset.getString(6), 
+									rset.getString(7), 
+									rset.getString(8), 
+									rset.getLong(9));
 			}
-			return member;
+		} finally {
+			DataSourceManager.close(conn, pstmt, rset);
 		}
+		return member;
+	}
 }
