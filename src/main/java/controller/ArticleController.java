e74fcfe0469a549b73eeb2e7249e8d2822df119c
DoomchitYJ <poj1004@inha.edu>
2025-02-07 11:38:13 +0900

    feat: controller 구현

---
+++ src/main/java/controller/ArticleController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.io.IOException;
+
+import controller.action.Action;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/article")
+public class ArticleController extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String command = request.getParameter("command");
+		if(command == null) {
+			command = "list";   //모든 방명록 보기
+		}
+		
+		ActionFactory af = ActionFactory.getInstance();
+	
+		Action action = af.getAction(command);  //list, view, updateForm, write, update
+		action.execute(request, response);
+	}	
+}
