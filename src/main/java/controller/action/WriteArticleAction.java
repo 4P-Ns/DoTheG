02466732b1059a1761f215e3658b1013819e4ba3
DoomchitYJ <poj1004@inha.edu>
2025-02-10 20:58:03 +0900

    fix: update & write 오류 수정

--- src/main/java/controller/action/WriteArticleAction.java
+++ src/main/java/controller/action/WriteArticleAction.java
@@ -11,15 +11,13 @@ import model.dao.MemberDAO;
 import model.domain.Article;
 import model.domain.Member;
 
-public class WriteArticleAction {
+public class WriteArticleAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
 		String title = request.getParameter("title");
 		String content = request.getParameter("content");
 
 		try {
-			if (title == null || title.trim().length() == 0 || title == null || content.trim().length() == 0
-					|| content == null || content.trim().length() == 0) {
+			if (title == null || title.trim().length() == 0 || content == null || content.trim().length() == 0) {
 				throw new Exception("입력값이 충분하지 않습니다.");
 			}
 
@@ -27,23 +25,23 @@ public class WriteArticleAction {
 			String id = null;
 			if (cookies != null) {
 				for (Cookie c : cookies) {
-					if (c.getName().equals("id")) {
+					if (c.getName().equals("userId")) {
 						id = c.getValue();
 						break;
 					}
 				}
 			}
-
 			Member member = MemberDAO.getMember(id);
 
 			boolean result = ArticleDAO.writeArticle(member, new Article(title, content));
 
 			if (result) {
-				response.sendRedirect("article?command=list");
+				response.sendRedirect("article?command=board");
 			} else {
 				throw new Exception("입력값이 충분하지 않습니다.");
 			}
 		} catch (Exception e) {
+			e.printStackTrace();
 			request.setAttribute("errorMsg", e.getMessage());
 			request.getRequestDispatcher("view/error.jsp").forward(request, response);
 		}
