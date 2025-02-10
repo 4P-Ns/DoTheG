abf49a7386e3fd5d864d89f3be67aea4fe9fcf52
Jeongho427 <fgh2585@gmail.com>
2025-02-10 10:29:53 +0900

    fix: 글 목록 css 추가

--- src/main/java/controller/action/AllArticleViewAction.java
+++ src/main/java/controller/action/AllArticleViewAction.java
@@ -2,11 +2,15 @@ package controller.action;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dao.ArticleDAO;
+import model.dao.MemberDAO;
+import model.domain.Article;
+import model.dto.ArticleDTO;
 
 public class AllArticleViewAction implements Action {
  
@@ -14,7 +18,15 @@ public class AllArticleViewAction implements Action {
 		String url = "view/error.jsp";
 		
 		try {
-			request.setAttribute("articles", ArticleDAO.getAllArticles());
+			ArrayList<ArticleDTO> articleList = new ArrayList<>();
+			ArrayList<Article> articles = ArticleDAO.getAllArticles();
+			
+			for(Article a:articles) {
+				String authorName = MemberDAO.getMemberByPk(a.getAuthorId()).getNickname();
+				articleList.add(new ArticleDTO(a,authorName));
+			}
+			
+			request.setAttribute("articles", articleList);
 			url = "view/list.jsp";
 		} catch (SQLException e) {
 			request.setAttribute("errorMsg", "모든 게시글 검색시 문제 발생, 잠시후 재 시도 하세요");
