197f9e880f260e1d156fcbf121ac8489a4eaa15d
DoomchitYJ <poj1004@inha.edu>
2025-02-10 20:10:39 +0900

    fix: 게시글 삭제 기능 오류 수정

--- src/main/java/controller/action/DeleteArticleAction.java
+++ src/main/java/controller/action/DeleteArticleAction.java
@@ -13,35 +13,25 @@ public class DeleteArticleAction implements Action{
 
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String articleId = request.getParameter("article_id");
-		String authorId = request.getParameter("author_id");
-		
-		String id = null;
-		Cookie[] cookies = request.getCookies();
-		if(cookies!=null) {
-			for(Cookie c:cookies) {
-				if(c.getName().equals("id")) {
-					id = c.getValue();
-					break;
-				}
-			}
-		}
 		
 		try{
-			if(articleId == null || articleId.trim().length() == 0 || id == null || id.trim().length() == 0){
+			if(articleId == null || articleId.trim().length() == 0){
 				throw new Exception("입력값이 충분하지 않습니다.");
 			}
 			
-			boolean result = ArticleDAO.deleteArticle(Long.parseLong(articleId),Long.parseLong(authorId));
+			boolean result = ArticleDAO.deleteArticle(Long.parseLong(articleId));
 
 			if(result){
-				response.sendRedirect("board?command=list");
+				response.sendRedirect("article?command=board");
 			}else{
 				throw new Exception("이미 삭제된 게시물이거나, 비밀번호가 올바르지 않습니다.");
 			}
 		}catch (SQLException e) {
+			e.printStackTrace();
 			request.setAttribute("errorMsg", "시스템 문제가 발생했습니다.");
 			request.getRequestDispatcher("view/error.jsp").forward(request, response);
 		}catch (Exception e){
+			e.printStackTrace();
 			request.setAttribute("errorMsg", e.getMessage());
 			request.getRequestDispatcher("view/error.jsp").forward(request, response);
 		}
